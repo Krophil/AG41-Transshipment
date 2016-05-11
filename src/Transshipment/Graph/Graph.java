d75397e456f856f8b841f43b8415759d093b5e4b
Esia Belbachir <meith_moon_paradox@yahoo.fr>
2016-05-11 15:52:46 +0200

    corrected genericity problem

--- src/Transshipment/Graph/Graph.java
+++ src/Transshipment/Graph/Graph.java
@@ -4,50 +4,50 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-public class Graph<Node, Edge> {
-	private HashMap<Integer, Node> nodes;
-	private HashMap<Integer, HashMap<Integer, Edge>> edges;
+public class Graph<N, E> {
+	private HashMap<Integer, N> nodes;
+	private HashMap<Integer, HashMap<Integer, E>> edges;
 	
 	public Graph() {
 		nodes = new HashMap<>();
 		edges = new HashMap<>();
 	}
 	
-	public Node getNode(int i) {
+	public N getNode(int i) {
 		return nodes.get(i);
 	}
 	
-	public ArrayList<Edge> getOutEdges(int i) {
-		return new ArrayList<Edge>(edges.get(i).values());
+	public ArrayList<E> getOutEdges(int i) {
+		return new ArrayList<E>(edges.get(i).values());
 	}
 	
-	public LinkedList<Edge> getInEdges(int i) {
-		LinkedList<Edge> ed = new LinkedList<>();
+	public LinkedList<E> getInEdges(int i) {
+		LinkedList<E> ed = new LinkedList<>();
 		for (int n : nodes.keySet()) {
-			Edge e = edges.get(n).get(i);
+			E e = edges.get(n).get(i);
 			if (e != null)
 				ed.add(e);
 		}
 		return ed;
 	}
 	
-	public Edge getEdge(int i,  int j) {
+	public E getEdge(int i,  int j) {
 		if (edges.get(i) != null) {
 			return edges.get(i).get(j);
 		}
 		return null;	
 	}
 	
-	public boolean addNode(int i, Node n) {
+	public boolean addNode(int i, N n) {
 		if (!nodes.containsKey(i)) {
 			nodes.put(i, n);
-			edges.put(i, new HashMap<Integer, Edge>());
+			edges.put(i, new HashMap<Integer, E>());
 			return true;
 		}
 		return false;
 	}
 	
-	public boolean addEdge(int i, int j, Edge e) {
+	public boolean addEdge(int i, int j, E e) {
 		if (nodes.containsKey(i) && nodes.containsKey(j) && !edges.get(i).containsKey(j)) {
 			edges.get(i).put(j, e);
 			return true;
