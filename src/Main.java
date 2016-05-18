36aa64524719ca736d4e80fd0356aca87a70db1e
Esia Belbachir <meith_moon_paradox@yahoo.fr>
2016-05-18 11:52:52 +0200

    file and solving time set as program arguments.

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ import solver.Solver;
 public class Main {
 
 	public static void main(String[] args) {
-		if (args.length == 1) {
+		if (args.length == 2) {
 			Solver solv = new Solver();
-			solv.solve(args[0], 2000);
+			solv.solve(args[0], Integer.valueOf(args[1]));
 		} else {
-			System.out.println("Missing argument : file name");
+			System.out.println("Missing arguments : file name and solving time (ms)");
 		}
 	}
 
